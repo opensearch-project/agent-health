b29c19cce2761c797df4a4f8d35540eb2f6423f2
Megha Goyal <goyamegh@amazon.com>
2025-12-31 00:13:52 -0800

    remove dashboardsTraces plugin and related files
    Signed-off-by: Megha Goyal <goyamegh@amazon.com>

--- common/index.ts
+++
@@ -1,2 +0,0 @@
-export const PLUGIN_ID = 'dashboardsTraces';
-export const PLUGIN_NAME = 'dashboardsTraces';
--- server/types.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface DashboardsTracesPluginSetup {}
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface DashboardsTracesPluginStart {}
